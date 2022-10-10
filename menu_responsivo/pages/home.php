5d759cd87c13ef919c761168b7e8fecd68b2158e
Renato Azevedo <renato.azevedo0001@gmail.com>
2022-10-10 13:07:15 -0300

    Segue

--- menu_responsivo/pages/home.php
+++ menu_responsivo/pages/home.php
@@ -5,13 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
-    <link rel="stylesheet" href="./style4.css">
+    <link rel="stylesheet" href="../style.css">
 </head>
 <body>
-    <div class="home-container">
+    <div style="color:white;padding:30px">
         <?php
-            echo "<h1>Home</h1>";
-            echo "<h3>Sou do Rio de Janeiro </h3>"
+            
+            echo "<h1>Objetivo do site é de apresentar de maneira SIMPLES:</h1>";
+            echo "<br>";
+            echo "<h4>1. Acessos a APIs </h4>";
+            echo "<h4>2. Como criar URLs amigáveis </h4>";
+            echo "<h4>3. Como criar um menu Responsivo</h4>";
+            echo "<h4>4. Utilização de animação de Menu e Sub-menu com JS</h4>";
+            echo"<br><br>";
+            echo "<h2>Espero que gostem !!!<h2>"
+
         ?>
     </div>
 </body>
