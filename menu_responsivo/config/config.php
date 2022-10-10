ea1638f53b373f1a66cd884e87e3dad26b3f0d75
Renato Azevedo <97203729+Renato-Azevedo-01@users.noreply.github.com>
2022-10-10 13:18:00 -0300

    Update config.php

--- menu_responsivo/config/config.php
+++ menu_responsivo/config/config.php
@@ -1,4 +1,4 @@
 <?php 
-define('chave_hg_api','490c5afe');
-define('cidade', 455825);
+define('chave_hg_api','sua chave');
+define('cidade', codigo da cidade ); ->caso queira saber o tempo de qualquer cidade ou o sistema pega automático por sua localização;
 ?>
